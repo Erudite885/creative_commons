8f1b888207271589d943b322a66de8257d13dee5
erudite885 <uchebianca@gmail.com>
2023-07-20 04:05:13 +0100

    auth, conversation and index routes added

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+export { default as userRoute } from "./user.route.js";
+export { default as gigRoute } from "./gig.route.js";
+export { default as conversationRoute } from "./converstaion.route.js";
+export { default as messageRoute } from "./message.route.js";
+export { default as orderRoute } from "./order.route.js";
+export { default as reviewRoute } from "./review.route.js";
+export { default as authRoute } from "./auth.route.js";
