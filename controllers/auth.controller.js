5b10ce83d222349077e48d6955913e75bbd648fb
erudite885 <uchebianca@gmail.com>
2023-07-21 02:05:52 +0100

    files added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,9 @@
-import User from "../models/index.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
@@ -12,26 +13,37 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("a new user has been created.");
   } catch (error) {
-    res.status(500).send("something went wrong");
+    next(error);
   }
 };
 
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(404).send("User not found!");
+    const err = new Error();
+    err.status = 404;
+    err.message = "User not found";
+    if (!user) return next(createError(404, "User not found"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect)
       return res.status(400).send("incorrect username or password!");
 
-    const token = jwt.sign({
-      id: user._id,
-      isSeller: user.isSeller,
-    });
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
     const { password, ...info } = user._doc;
-    res.status(200).send(info);
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (error) {
     res.status(500).send("something went wrong");
   }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 export { default as userRoute } from "./user.route.js";
 export { default as gigRoute } from "./gig.route.js";
-export { default as conversationRoute } from "./converstaion.route.js";
+export { default as conversationRoute } from "./conversation.route.js";
 export { default as messageRoute } from "./message.route.js";
 export { default as orderRoute } from "./order.route.js";
 export { default as reviewRoute } from "./review.route.js";
