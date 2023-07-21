0e2990a5a1c4c0ed3187be09a29f1b1e7a34b0e0
erudite885 <uchebianca@gmail.com>
2023-07-21 09:57:11 +0100

    jwt middleware added

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,11 +1,14 @@
-export const verifyToken = (req, res) => {
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError";
+
+export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("you are not authenticated");
+  if (!token) return next(createError(401, "You are not authenticated!"));
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-      if(err) return res.status(403).send("Token not valid!")
-      req.userId = payload.id;
-      req.isSeller = payload.isSeller
-      
-    });
+    if (err) return next(createError(403, "Token is not valid"));
+    req.userId = payload.id;
+    req.isSeller = payload.isSeller;
+    next();
+  });
 };
