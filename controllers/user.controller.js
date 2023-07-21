9165f6ed0b1542c25c8c301e26edc6ede7a0f321
erudite885 <uchebianca@gmail.com>
2023-07-21 02:04:06 +0100

    user controller modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,12 @@
-export const fn = (rer, res) => {
-  //
-//   res.send("from controller side");
+import User from "../models/user.model.js";
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (req.userId !== user._id.toString()) {
+    return res.status(403).send("you are not authorized to delete this item.");
+  }
+
+  await User.findByIdAndDelete(req.params.id);
+  res.status(200).send("deleted successfully.");
 };
