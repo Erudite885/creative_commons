f00c0e0f2ea8f0dc558ed5d05cfbd9eb940b3981
erudite885 <uchebianca@gmail.com>
2023-07-23 09:47:10 +0100

    routes and middleware imported

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoute from "./routes/auth.route.js";
-// import conversationRoute from "./routes/conversation.route.js";
+import conversationRoute from "./routes/conversation.route.js";
 import gigRoute from "./routes/gig.route.js";
-// import messageRoute from "./routes/message.route.js";
-// import orderRoute from "./routes/order.route.js";
-// import reviewRoute from "./routes/review.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
 import userRoute from "./routes/user.route.js";
 
 const app = express();
@@ -32,10 +32,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
-// app.use("/api/orders", orderRoute);
-// app.use("/api/conversations", conversationRoute);
-// app.use("/api/messages", messageRoute);
-// app.use("/api/reviews", reviewRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
