108d3325e2f7adb1bb577e901167db67f08282fa
erudite885 <uchebianca@gmail.com>
2023-07-25 04:45:43 +0100

    routes update

--- routes/review.route.js
+++ routes/review.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyToken } from "../middleware/jwt";
+import { verifyToken } from "../middleware/jwt.js";
 import {
   createReview,
   getReviews,
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 
 router.post("/", verifyToken, createReview);
-router.get("/:id", getReviews);
+router.get("/:gigId", getReviews);
 router.delete("/:id", verifyToken, deleteReview);
 
 export default router;
